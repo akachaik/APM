cddc319939fe88aa393abd768f5f097146e9bc42
kit <308tube@gmail.com>
2016-04-13 02:52:02 +0700

    Subscribing to an Observable

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { ProductListComponent } from './products/product-list.component';
 import { ProductService } from './products/product.service';
 
 import { HTTP_PROVIDERS } from 'angular2/http';
-import 'rxjs/RX'; // Load all features
+import 'rxjs/Rx'; // Load all features
 
 @Component({
     selector: 'pm-app',
