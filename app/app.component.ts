65519bccafee700e12d9976a61979a10bcfa0169
kit <308tube@gmail.com>
2016-04-13 22:14:54 +0700

    Tying Routes to Actions

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,22 +5,30 @@ import { ProductService } from './products/product.service';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx'; // Load all features
 
-import { ROUTER_PROVIDERS, RouteConfig }  from 'angular2/router';
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES }  from 'angular2/router';
 import { WelcomeComponent } from './home/welcome.component';
 
 @Component({
     selector: 'pm-app',
     template: `
     <div>
-        <h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
-    </div>`,
-    directives: [ProductListComponent],
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['Welcome']">Home</a></li>
+                    <li><a [routerLink]="['Products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    `,
+    directives: [ROUTER_DIRECTIVES],
     providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
 @RouteConfig([
     { path: '/welcome', name: 'Welcome', component : WelcomeComponent, useAsDefault: true },
-    { path: '/products', name: 'Products', component : ProductListComponent, useAsDefault: true },
+    { path: '/products', name: 'Products', component : ProductListComponent }
 ])
 export class AppComponent {
     pageTitle: string = "Acme Product Management";
