37fa2b59ad71ed0ccdbe09d54ba37e8e1f346a19
kit <308tube@gmail.com>
2016-04-14 01:45:26 +0700

    Implement detail page

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,15 +1,30 @@
 import { Component } from 'angular2/core';
 import { RouteParams, Router } from 'angular2/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { StarComponent } from './../shared/star.component';
 
 @Component({
-    templateUrl: 'app/products/product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
 })
 export class ProductDetail {
     pageTitle: string = "Product Details";
+    product: IProduct;
+    productId: number;
+    errorMessage: string;
     
-    constructor(private _routeParams: RouteParams, private _router: Router) {
-        let id = +this._routeParams.get('id');
-        this.pageTitle += ` : ${id}`;
+    constructor(private _routeParams: RouteParams, 
+                private _router: Router,
+                private _productService: ProductService) {
+        this.productId = +this._routeParams.get('id');
+        this.pageTitle += ` : ${this.productId}`;
+    }
+    
+    ngOnInit(): void {
+        this._productService.getProduct(this.productId).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
     }
     
     onBack(): void {
