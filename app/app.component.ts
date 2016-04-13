284beec797233124f5efc55a3a48ce87bedbd853
kit <308tube@gmail.com>
2016-04-13 21:49:25 +0700

    Configuring Routes ( Welcome route and ProductList route)

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { ProductService } from './products/product.service';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx'; // Load all features
 
-import { ROUTER_PROVIDERS }  from 'angular2/router';
+import { ROUTER_PROVIDERS, RouteConfig }  from 'angular2/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @Component({
     selector: 'pm-app',
@@ -17,6 +18,10 @@ import { ROUTER_PROVIDERS }  from 'angular2/router';
     directives: [ProductListComponent],
     providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    { path: '/welcome', name: 'Welcome', component : WelcomeComponent, useAsDefault: true },
+    { path: '/products', name: 'Products', component : ProductListComponent, useAsDefault: true },
+])
 export class AppComponent {
     pageTitle: string = "Acme Product Management";
 }
